ccc54437a86b42b951f6f294eda613e6d605ce24
togethrstore <developer@togethr.store>
2024-04-15 05:40:28 +0530

    initial commit

---
+++ app/bloodbank/addbb-id/page.js
@@ -0,0 +1,20 @@
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Link } from 'next-view-transitions'
+
+export default function page() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+      <Link href="/"><Button>Home</Button></Link>
+
+      <h1>Blood bank first page</h1>
+
+      <div className="flex flex-col gap-3 w-2/3">
+        <h1 className="font-semibold">Enter blood id</h1>
+      <Input className="shadow-md"/>
+      <Button>Submit</Button>
+      </div>
+    </div>
+  )
+}
